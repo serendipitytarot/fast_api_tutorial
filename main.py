177ba827da3e91190101c3390101a5db045674d6
KrisChuang <kris.kh.chuang@gmail.com>
2024-10-09 00:41:37 +0800

    add Connect DB

--- main.py
+++ main.py
@@ -6,6 +6,14 @@ import os
 
 from dotenv import load_dotenv
 
+import psycopg2
+
+db_name = 'kris'
+db_user = 'kris'
+db_password = 'SJuALVOkZGDG8lqPsi8rVGU5AyQRuWNS'
+db_host = 'dpg-cs1trb08fa8c73d4p4u0-a.oregon-postgres.render.com'
+db_port = 5432
+
 load_dotenv()
 
 key = os.getenv("FAKE_VALUE")
@@ -22,3 +30,20 @@ async def main():
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Union[str, None] = None):
     return {"item_id": item_id, "q":q}
+
+@app.get("/getdata/{location_name}")
+def get_date(location_name: Union[str, None] = None):
+    conn = psycopg2.connect(
+        database=db_name,
+        user=db_user,
+        password=db_password,
+        host=db_host,
+        port=db_port
+    )
+
+    cur = conn.cursor()
+    select_query = "select * from weather where city = '"+location_name+"' "
+    cur.execute(select_query)
+    records = cur.fetchall()
+
+    return {"city": records[0][0], "low": records[0][1], "high": records[0][2]}
